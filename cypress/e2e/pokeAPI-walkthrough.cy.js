20ee73d9351261c4e3358748ef78a45e6776468c
iam08160 <imackenzie81691@gmail.com>
2025-03-14 11:05:28 -0400

    refactored for dynamic testing/comments added

--- cypress/e2e/pokeAPI-walkthrough.cy.js
+++ cypress/e2e/pokeAPI-walkthrough.cy.js
@@ -1,43 +1,62 @@
 describe("Full App Runthrough", () => {
-    beforeEach(() => {
+  beforeEach(() => {
+    //function to visit local host before each test
     cy.visit("localhost:5173");
   });
+
   it("page load", () => {
+    //verifying Search component is rendered
     cy.get('[data-testid="cypress-input"]').should("exist");
     cy.get('[data-testid="cypress-catch"]').should("exist");
     cy.get('[data-testid="cypress-clear"]').should("exist");
   });
+  //searching for pokemon
   it("Catch Mon", () => {
+    //grabbing the api call and assigning 'alias' to getPikachu
+    cy.intercept("GET", "https://pokeapi.co/api/v2/pokemon/pikachu").as("getPikachu");
+    //user types pikachu in the input field
     cy.get('[data-testid="cypress-input"]').type("pikachu");
+    //user clicks catch
     cy.get('[data-testid="cypress-catch"]').click();
-    cy.request("https://pokeapi.co/api/v2/pokemon/pikachu").then((response) => {expect(response.status).to.eq(200)});
-    cy.get('[data-testid="cypress-name"]').should("exist");
-    cy.get('[data-testid="cypress-name"]').should("contain", "pikachu");
-    cy.get('[data-testid="cypress-height"]').should("exist");
-    cy.get('[data-testid="cypress-weight"]').should("exist");
+    // using alias and waiting for successfull API response. Variables assigned to the response body
+    cy.wait("@getPikachu").then((i) => {
+      const { name, height, weight, sprites } = i.response.body;
+      //confirming the name, height, weight and image are displayed dynamically
+      cy.get('[data-testid="cypress-name"]').should("exist").and("contain", name);
+      cy.get('[data-testid="cypress-image"]').should("exist").and("have.attr", "src", sprites.front_default);
+      cy.get('[data-testid="cypress-height"]').should("exist").and("contain", `Height: ${height}`);
+      cy.get('[data-testid="cypress-weight"]').should("exist").and("contain", `Weight: ${weight}`);
+    });
   });
+  //clearing pokemon
   it("Clear Mon", () => {
+    cy.intercept("GET", "https://pokeapi.co/api/v2/pokemon/charmander").as("getCharmander");
     cy.get('[data-testid="cypress-input"]').type("charmander");
     cy.get('[data-testid="cypress-catch"]').click();
-    cy.request("https://pokeapi.co/api/v2/pokemon/charmander").then((response) => {expect(response.status).to.eq(200)});
-    cy.get('[data-testid="cypress-name"]').should("exist");
-    cy.get('[data-testid="cypress-name"]').should("contain", "charmander");
-    cy.get('[data-testid="cypress-image"]').should("exist");
-    cy.get('[data-testid="cypress-height"]').should("exist");
-    cy.get('[data-testid="cypress-weight"]').should("exist");
+    cy.wait("@getCharmander").then((i) => {
+      const { name, height, weight, sprites } = i.response.body;
+      cy.get('[data-testid="cypress-name"]').should("exist").and("contain", name);
+      cy.get('[data-testid="cypress-image"]').should("exist").and("have.attr", "src", sprites.front_default);
+      cy.get('[data-testid="cypress-height"]').should("exist").and("contain", `Height: ${height}`);
+      cy.get('[data-testid="cypress-weight"]').should("exist").and("contain", `Weight: ${weight}`);
+    });
+    // use clicks clear
     cy.get('[data-testid="cypress-clear"]').click();
+    // elements are removed 
     cy.get('[data-testid="cypress-name"]').should("not.exist");
     cy.get('[data-testid="cypress-image"]').should("not.exist");
     cy.get('[data-testid="cypress-height"]').should("not.exist");
     cy.get('[data-testid="cypress-weight"]').should("not.exist");
   });
+  //error handling
   it("No Mon Found", () => {
+    cy.intercept("GET", "https://pokeapi.co/api/v2/pokemon/chuck%20norris").as("getChuckNorris");
+    // user types in a non existent pokemon
     cy.get('[data-testid="cypress-input"]').type("chuck norris");
     cy.get('[data-testid="cypress-catch"]').click();
-    cy.get('[data-testid="cypress-fetch-error"]').should("exist");
-    cy.get('[data-testid="cypress-fetch-error"]').should(
-      "contain",
-      "Mon not found"
-    );
+    //failed API call
+    cy.wait("@getChuckNorris").its("response.statusCode").should("eq", 404);
+    //error is displayed
+    cy.get('[data-testid="cypress-fetch-error"]').should("exist").and("contain", "Mon not found");
   });
 });
