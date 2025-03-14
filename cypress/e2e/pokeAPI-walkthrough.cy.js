3a4cfaa1a2ca9ec0a0bc52898551e3c60e8dab37
iam08160 <imackenzie81691@gmail.com>
2025-03-14 10:20:44 -0400

    reformat with beforeEach and added API test

--- cypress/e2e/pokeAPI-walkthrough.cy.js
+++ cypress/e2e/pokeAPI-walkthrough.cy.js
@@ -1,24 +1,26 @@
 describe("Full App Runthrough", () => {
-  it("page load", () => {
+    beforeEach(() => {
     cy.visit("localhost:5173");
+  });
+  it("page load", () => {
     cy.get('[data-testid="cypress-input"]').should("exist");
     cy.get('[data-testid="cypress-catch"]').should("exist");
     cy.get('[data-testid="cypress-clear"]').should("exist");
   });
   it("Catch Mon", () => {
-    cy.visit("localhost:5173");
     cy.get('[data-testid="cypress-input"]').type("pikachu");
     cy.get('[data-testid="cypress-catch"]').click();
-    cy.get('[data-testid="cypress-name"]').should("exist")
+    cy.request("https://pokeapi.co/api/v2/pokemon/pikachu").then((response) => {expect(response.status).to.eq(200)});
+    cy.get('[data-testid="cypress-name"]').should("exist");
     cy.get('[data-testid="cypress-name"]').should("contain", "pikachu");
     cy.get('[data-testid="cypress-height"]').should("exist");
     cy.get('[data-testid="cypress-weight"]').should("exist");
   });
   it("Clear Mon", () => {
-    cy.visit("localhost:5173");
     cy.get('[data-testid="cypress-input"]').type("charmander");
     cy.get('[data-testid="cypress-catch"]').click();
-    cy.get('[data-testid="cypress-name"]').should("exist")
+    cy.request("https://pokeapi.co/api/v2/pokemon/charmander").then((response) => {expect(response.status).to.eq(200)});
+    cy.get('[data-testid="cypress-name"]').should("exist");
     cy.get('[data-testid="cypress-name"]').should("contain", "charmander");
     cy.get('[data-testid="cypress-image"]').should("exist");
     cy.get('[data-testid="cypress-height"]').should("exist");
@@ -30,11 +32,12 @@ describe("Full App Runthrough", () => {
     cy.get('[data-testid="cypress-weight"]').should("not.exist");
   });
   it("No Mon Found", () => {
-    cy.visit("localhost:5173");
-    cy.get('[data-testid="cypress-input"]').type("butt");
+    cy.get('[data-testid="cypress-input"]').type("chuck norris");
     cy.get('[data-testid="cypress-catch"]').click();
-    cy.get('[data-testid="cypress-fetch-error"]').should("exist")
-    cy.get('[data-testid="cypress-fetch-error"]').should("contain", "Mon not found");
-    
+    cy.get('[data-testid="cypress-fetch-error"]').should("exist");
+    cy.get('[data-testid="cypress-fetch-error"]').should(
+      "contain",
+      "Mon not found"
+    );
   });
 });
